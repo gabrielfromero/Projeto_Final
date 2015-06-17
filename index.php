52e577eb8bb0341c9a5af1d7de994ce6f46bd12e
Gabriel <gabrielfaber1@hotmail.com>
2015-06-16 21:34:44 -0300

    Projeto aula 16.06

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+?>
+
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="ISO-8859-1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
+        <script src="js/jquery-1.11.3.js" type="text/javascript"></script>
+        <script src="js/bootstrap.js" type="text/javascript"></script>        
+    </head>
+    <body>
+        <div align="center" style="font-family: serif"> <h1>Digital Computer</h1></div>
+        <div class="container" src="img/Textura.jpg">
+
+            <div class="row" style="margin: 15px">
+                <div class="col-xs-6 col-sm-4"><h2>Computadores</h2></div>
+                <div class="col-xs-6 col-sm-4"><h2>Peças</h2></div>
+                <div class="clearfix visible-xs-block"></div>
+                <div class="col-xs-6 col-sm-4"><h2>Suporte</h2></div>
+            </div>
+            <div class="col-md-6" style="margin: 15px" >
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">Usuário</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="login" id="input-nome" class="form-control" placeholder="Login">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">Senha</label>
+                    <div class="col-sm-10">
+                        <input type="password" name="senha" id="input-endereco" class="form-control" placeholder="Senha">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+            </div>
+        </div>
+    </body>
+</html>
+
