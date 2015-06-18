3557fd4c6addfc32d64c8d40dbc878aeb1bcede4
Gabriel <gabrielfaber1@hotmail.com>
2015-06-18 20:31:56 -0300

    Projeto aula 18.06

--- index.php
+++ index.php
@@ -9,79 +9,99 @@
         <script src="js/bootstrap.js" type="text/javascript"></script>        
     </head>
     <body>
-        <div align="center" style="color: blue; font-family: fantasy"><h1>Digital Computer</h1></div>
+        <!--<div align="center" style="color: cyan; font-family: cursive"><h1><b>Digital Computer</b></h1></div>-->
+        <div align="center">
+            <img src="img/logo1.gif" style="border-radius: 40px">
+        </div>
+
+        <div><marquee style="font-family: arial;" slide direction="left"><span style="color: fuchsia;">
+                    <h1><b>A Melhor Empresa da Galaxia. Os Melhores Produtos com o Melhor Suporte do Mercado!
+                        </b></h1></span></marquee></div>
+
         <div class="container"
-             <div class="col-md-12" style="margin: 10px;" align= "center">
+             <head>
+            <div class="col-md-11" style="margin: 10px">
                 <ul>
-                    <li class="btn-primary col-md-5"> 
+                    <li class="btn-primary col-md-4" align="center" style="text-decoration-color: #28a4c9" 
                         <span class="icon-bar">
                             <a href="http://www.americanas.com.br/linha/267889/informatica/computadores-e-all-in-one"style="color:#000"><h2>Computadores</h2></a></span>
                     </li>
-                    <li class="btn-primary col-md-3">
+                    <li class="btn-primary col-md-4" align="center">
                         <span class="icon-bar">
                             <a href="https://www.balaodainformatica.com.br/Produtos/Inform%C3%A1tica/Componentes-e-Pe%C3%A7as"style="color: #000"><h2>Peças</h2></a></span>
                     </li>
-                    <li class="btn-primary col-md-4">
+                    <li class="btn-primary col-md-4" align="center">
                         <span class="icon-bar">
                             <a href="http://www.smartsupport.com.br/" style="color: #000"><h2>Suporte</h2></a></span>
                     </li>
                 </ul>
             </div>
-            <div align="center"  class="col-lg-4" style="margin: 10px">
-                <div class="jumbotron">
-                    <div class="form-group">
-                        <label class="col-sm-4 control-label" id="user">Usuário</label>
-                        <div class="col-sm-10">
-                            <input type="text" name="login" class="form-control" placeholder="Login">
-                        </div>
+        </head>
+        <div align="center"  class="col-lg-4" style="margin: 10px">
+            <div class="jumbotron">
+                <div class="form-group">
+                    <label class="col-sm-4 control-label" id="user">Usuário</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="login" class="form-control" placeholder="Login">
                     </div>
-                    <div class="form-group">
-                        <label class="col-sm-4 control-label" id="senha">Senha</label>
-                        <div class="col-sm-10">
-                            <input type="password" name="senha"class="form-control" placeholder="Senha">
-                        </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-4 control-label" id="senha">Senha</label>
+                    <div class="col-sm-10">
+                        <input type="password" name="senha"class="form-control" placeholder="Senha">
                     </div>
-                    <br>
-                    <button  type="button" class="btn btn-success" id="login">Entrar</button>
-                    <br>
-                    <br>
-                    <br>
                 </div>
-                <div class="row" style="background-color: #bce8f1" align="center">
-                    <h2>Carrinho de Compras</h2>
-                    <br>
-                    <ul>
-                        <li><h4>Produto 1</h4></li>
-                        <br>
-                        <li><h4>Produto 2</h4></li>
-                        <br>
-                        <li><h4>Produto 3</h4></li>
-                        <br>
-                        <li><h4>Produto 4</h4></li>
-                        <br>
-                        <li><h4>Produto 5</h4></li>
-                    </ul>
-                </div>
+                <br>
+                <button  type="button" class="btn btn-success" id="login">Entrar</button>
+                <br>
+                <br>
+                <br>
             </div>
-            <div class="col-md-7"  style="margin: 10px">
-                <div class="row" style="background-color: darkcyan">
-                    <ul><h3 style="font-family: inherit">Lançamentos</h3></ul>
-                    <ul>
-                        <li><h4>Computador Dell i10</h4>
-                            <img src="img/pc_1.jpg" height="200">
-                            <button type="button" id="compra1" class="btn btn-danger">Comprar</button> 
-                            <h5>Este Computador é uma super máquina da revolução moderna, com 50Gb de Memória Ram e seu processador de 10 núcleos de processameto.</h5>
-                        </li>
-                        <li><h4>Computador Lenovo i5</h4>
-                            <img src="img/pc_2.jpg" height="250">
-                            <button type="button" id="compra2" class="btn btn-danger">Comprar</button>
-                            <h5>Esta máquina de uso pessoal é perfeita para o seu dia-a-dia com seu design futuristico e seu poder de armazento altíssimo.</h5>
-                        </li>
-                    </ul>
-                </div>
+            <div class="row" style="background-color: #bce8f1" align="center">
+                <h2>Carrinho de Compras</h2>
+                <br>
+                <ul>
+                    <a><li><h4>Produto 1</h4></li></a>
+                    <br>
+                    <a><li><h4>Produto 2</h4></li></a>
+                    <br>
+                    <a><li><h4>Produto 3</h4></li></a>
+                    <br>
+                    <a><li><h4>Produto 4</h4></li></a>
+                    <br>
+                    <a><li><h4>Produto 5</h4></li></a>
+                </ul>
             </div>
         </div>
-        <script>
+        <div class="col-md-7"  style="margin: 10px">
+            <div class="row" style="background-color: darkcyan">
+                <ul><h3 style="font-family: inherit">Lançamentos</h3></ul>
+                <ul>
+                    <li><h4>Computador Dell i10</h4>
+                        <img src="img/pc_1.jpg" height="200">
+                        <button type="button" id="compra1" class="btn btn-danger">Comprar</button> 
+                        <h5>Este Computador é uma super máquina da revolução moderna, com 50Gb de Memória Ram e seu processador de 10 núcleos de processameto.</h5>
+                    </li>
+                    <li><h4>Computador Lenovo i5</h4>
+                        <img src="img/pc_2.jpg" height="250">
+                        <button type="button" id="compra2" class="btn btn-danger">Comprar</button>
+                        <h5>Esta máquina de uso pessoal é perfeita para o seu dia-a-dia com seu design futuristico e seu poder de armazento altíssimo.</h5>
+                    </li>
+                    <li><h4>Computador Apple</h4>
+                        <img src="img/pc_3.jpg" height="300">
+                        <button type="button" id="compra3" class="btn btn-danger">Comprar</button>
+                        <h5>Este é o mais novo lançamento da Apple, com seu super processador você poderá jogar os melhores jogos.</h5>    
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row" style="color: red" align="center">
+        <h3>Site criado em Julho de 2015 por [Gabriel Faber Romero]
+            <br> 
+            Copyright © 2003-2015. O design deste site é uma cortesia do [Mestre Gabriel].</h3>
+    </div>
+    <script>
 //                    $('#login').click(function () {
 //            var valido = true;
 //                    if ($('#user').val() == '') {
@@ -95,7 +115,7 @@
 //            }
 //            return valido;
 //            });
-        </script>
-    </body>
+    </script>
+</body>
 </html>
 
